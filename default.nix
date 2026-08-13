5b9b75681dc88c0f5ac2a82203439e5b432dc105
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-08-13 08:46:44 +0200

    feat: switch to nightly rust

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
-{pkgs ? import <nixpkgs> {}}: let
+{pkgs ? import <nixpkgs> {}, fenix, system}: let
   manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+  overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
 in
-  pkgs.rustPlatform.buildRustPackage rec {
+  (pkgs.makeRustPlatform {
+          inherit (fenix.packages.${system}.minimal) cargo rustc;
+        }).buildRustPackage rec {
     pname = manifest.name;
     version = manifest.version;
     cargoLock.lockFile = ./Cargo.lock;
@@ -12,6 +15,7 @@ in
     buildInputs = with pkgs; [openssl];
 
     # If you need to set environment variables for OpenSSL
+    RUSTC_VERSION = overrides.toolchain.channel;
     OPENSSL_DIR = pkgs.openssl.dev;
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
     OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
--- shell.nix
+++ shell.nix
@@ -1,17 +1,26 @@
-{pkgs ? import <nixpkgs> {}}:
+{pkgs ? import <nixpkgs> {}, stdenv, fenix, system}:
+let
+  overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+in
 pkgs.mkShell {
   # get dependencies from the main package
-  inputsfrom = [(pkgs.callPackage ./default.nix {})];
+  inputsfrom = [(pkgs.callPackage ./default.nix {inherit fenix system;})];
   # additional tooling
   buildinputs = with pkgs; [
-    rust-analyzer # lsp server
-    rustfmt # formatter
-    clippy # linter
+    rustup
+    rustPlatform.bindgenHook
     openssl
     pkg-config
-    cargo-deny
-    cargo-edit
-    cargo-watch
     just
   ];
+
+ buildInputs =
+      [
+      ];
+    RUSTC_VERSION = overrides.toolchain.channel;
+    # https://github.com/rust-lang/rust-bindgen#environment-variables
+    shellHook = ''
+      export PATH="''${CARGO_HOME:-~/.cargo}/bin":"$PATH"
+      export PATH="''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-${stdenv.hostPlatform.rust.rustcTarget}/bin":"$PATH"
+    '';
 }
